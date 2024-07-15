7229b938ed23522f6e2c47551bed277ce61711cb
puppydog4 <158152797+puppydog4@users.noreply.github.com>
2024-07-15 11:54:00 +0100

    add api with models and controllers for topics

---
+++ api/controllers/topics-controllers.js
@@ -0,0 +1,6 @@
+const { fetchTopics } = require("../models/topics-models");
+
+exports.getTopics = async (req, res, next) => {
+  const topics = await fetchTopics();
+  res.status(200).send({ topics: topics.rows });
+};
