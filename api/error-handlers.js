3de750fd6811ac631a407a4de8a9501bff62020f
puppydog4 <158152797+puppydog4@users.noreply.github.com>
2024-07-15 12:18:18 +0100

    add api routing and error handling middleware

---
+++ api/error-handlers.js
@@ -0,0 +1,5 @@
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status && err.message) {
+    res.status(err.status).send({ message: err.message });
+  } else next(err);
+};
--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics-controllers");
+const { handleCustomErrors, handleServerErrors } = require("./error-handlers");
 const app = express();
 
 app.get("/api/topics", getTopics);
 
+app.use(handleCustomErrors);
 module.exports = app;
