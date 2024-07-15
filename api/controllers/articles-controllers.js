546c7ee00273532668c888f7d188d975037293f8
puppydog4 <158152797+puppydog4@users.noreply.github.com>
2024-07-15 16:26:25 +0100

    add /api/articles endpoint - sends all articles sorted by date descending and adds a comment count key

--- api/controllers/articles-controllers.js
+++ api/controllers/articles-controllers.js
@@ -1,5 +1,15 @@
-const { fetchArticleById } = require("../models/articles-models");
-
+const {
+  fetchArticleById,
+  fetchArticles,
+} = require("../models/articles-models");
+exports.getArticles = async (req, res, next) => {
+  try {
+    const articles = await fetchArticles(next);
+    res.status(200).send({ articles });
+  } catch (error) {
+    next(error);
+  }
+};
 exports.getArticleById = async (req, res, next) => {
   const { article_id } = req.params;
   try {
