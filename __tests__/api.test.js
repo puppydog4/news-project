e1d2b94f71f26d8e130ae1b61e661d025d8f8319
puppydog4 <158152797+puppydog4@users.noreply.github.com>
2024-07-16 09:38:16 +0100

    add refactor for ticket 4-5

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -14,84 +14,67 @@ afterAll(async () => {
 });
 
 describe("/api/topics", () => {
-  it("GET: 200 sends an array of all topics", () => {
-    return request(app)
-      .get("/api/topics")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body).toEqual({ topics: seedData.topicData });
-      });
+  it("GET: 200 sends an array of all topics", async () => {
+    const { body } = await request(app).get("/api/topics").expect(200);
+    expect(body).toEqual({ topics: seedData.topicData });
   });
 });
 
 describe("/api", () => {
-  it("GET: 200 sends a json representation of all the available endpoints of the api", () => {
-    return request(app)
-      .get("/api")
-      .expect(200)
-      .then(({ body }) => {
-        expect(body).toEqual(endpoints);
-      });
+  it("GET: 200 sends a json representation of all the available endpoints of the api", async () => {
+    const { body } = await request(app).get("/api").expect(200);
+    expect(body).toEqual(endpoints);
   });
 });
+
 describe("/api/articles", () => {
-  it("GET: 200 send serves an array of all articles with a comment count , ordered by date descending by default", () => {
-    return request(app)
-      .get("/api/articles")
-      .expect(200)
-      .then(({ body }) => {
-        body.articles.forEach((article) =>
-          expect(article).toEqual({
-            author: expect.any(String),
-            title: expect.any(String),
-            article_id: expect.any(Number),
-            topic: expect.any(String),
-            created_at: expect.any(String),
-            votes: expect.any(Number),
-            article_img_url: expect.any(String),
-            comment_count: expect.any(String),
-          })
-        );
-        expect(body.articles).toBeSortedBy("created_at", { descending: true });
-      });
+  it("GET: 200 serves an array of all articles with a comment count, ordered by date descending by default", async () => {
+    const { body } = await request(app).get("/api/articles").expect(200);
+    body.articles.forEach((article) =>
+      expect(article).toEqual({
+        author: expect.any(String),
+        title: expect.any(String),
+        article_id: expect.any(Number),
+        topic: expect.any(String),
+        created_at: expect.any(String),
+        votes: expect.any(Number),
+        article_img_url: expect.any(String),
+        comment_count: expect.any(String),
+      })
+    );
+    expect(body.articles.length > 0).toBe(true);
+    expect(body.articles).toBeSortedBy("created_at", { descending: true });
   });
+
   describe("/api/articles/:article_id", () => {
-    it("GET: 200 sends the article selected by id", () => {
-      return request(app)
-        .get("/api/articles/2")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body).toEqual({
-            article: [
-              {
-                title: "Sony Vaio; or, The Laptop",
-                topic: "mitch",
-                author: "icellusedkars",
-                body: expect.any(String),
-                created_at: "2020-10-16T05:03:00.000Z",
-                votes: 0,
-                article_img_url:
-                  "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-              },
-            ],
-          });
-        });
+    it("GET: 200 sends the article selected by id", async () => {
+      const { body } = await request(app).get("/api/articles/2").expect(200);
+      expect(body).toEqual({
+        article: [
+          {
+            title: "Sony Vaio; or, The Laptop",
+            topic: "mitch",
+            author: "icellusedkars",
+            body: expect.any(String),
+            created_at: "2020-10-16T05:03:00.000Z",
+            votes: 0,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          },
+        ],
+      });
     });
-    it("GET: 404 sends an error when the article by id does not exist", () => {
-      return request(app)
-        .get("/api/articles/9999")
-        .expect(404)
-        .then(({ body }) => {
-          expect(body.message).toBe("Article by id: 9999 does not exist");
-        });
+
+    it("GET: 404 sends an error when the article by id does not exist", async () => {
+      const { body } = await request(app).get("/api/articles/9999").expect(404);
+      expect(body.message).toBe("Article by id: 9999 does not exist");
     });
-    it("GET: 400 sends an error when the id is invalid (not a number)", () => {
-      return request(app)
+
+    it("GET: 400 sends an error when the id is invalid (not a number)", async () => {
+      const { body } = await request(app)
         .get("/api/articles/badrequest")
-        .expect(400)
-        .then(({ body }) => {
-          expect(body.message).toBe("Bad Request");
-        });
+        .expect(400);
+      expect(body.message).toBe("Bad Request");
     });
   });
 });
--- api/models/articles-models.js
+++ api/models/articles-models.js
@@ -1,7 +1,6 @@
 const db = require("../../db/connection");
-exports.fetchArticles = async (next) => {
-  try {
-    const { rows } = await db.query(`SELECT 
+exports.fetchArticles = async () => {
+  const { rows } = await db.query(`SELECT 
       articles.author, 
       articles.title, 
       articles.article_id, 
@@ -26,26 +25,18 @@ exports.fetchArticles = async (next) => {
       articles.article_img_url
   ORDER BY 
       articles.created_at DESC;`);
-    return rows;
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+  return rows;
 };
 exports.fetchArticleById = async (id, next) => {
-  try {
-    const { rows } = await db.query(
-      "SELECT title , topic , author , body , created_at , votes , article_img_url FROM articles WHERE article_id = $1",
-      [id]
-    );
-    if (rows.length === 0) {
-      return Promise.reject({
-        status: 404,
-        message: `Article by id: ${id} does not exist`,
-      });
-    }
-    return rows;
-  } catch (err) {
-    next(err);
+  const { rows } = await db.query(
+    "SELECT title , topic , author , body , created_at , votes , article_img_url FROM articles WHERE article_id = $1",
+    [id]
+  );
+  if (rows.length === 0) {
+    return Promise.reject({
+      status: 404,
+      message: `Article by id: ${id} does not exist`,
+    });
   }
+  return rows;
 };
